b1dc34038104db305daa6cef3dda525f25c41af4
KiavashBahreini <kbsocialnetwork@gmail.com>
2019-02-11 00:31:12 +0100

    Update EmotionDetectionAssetSettings.cs

--- EmotionDetectionAsset/EmotionDetectionAssetSettings.cs
+++ EmotionDetectionAsset/EmotionDetectionAssetSettings.cs
@@ -13,9 +13,12 @@
  * and/or
  * Wim van der Vegt via wim.vandervegt [AT] ou [DOT] nl
  *
+ * Cite this work as:
+ * Bahreini, K., van der Vegt, W. & Westera, W. Multimedia Tools and Applications (2019). https://doi.org/10.1007/s11042-019-7250-z
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * This project has received funding from the European Union’s Horizon
+ * This project has received funding from the European Unionâ€™s Horizon
  * 2020 research and innovation programme under grant agreement No 644187.
  * You may obtain a copy of the License at
  *
